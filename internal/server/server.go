4b5ce4aaf35a2425d240bc54e2fc179d7ec075d5
DigiRon4ik <dr.digiron@gmail.com>
2025-02-11 20:38:57 +0300

    First version v0.1.0

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,9 @@
+// Package server contains the logic for setting up and running an HTTP server.
+// Includes route handling, middleware setup, and server configuration.
 package server
+
+// Config holds configuration values for the API server, such as host and port.
+type Config struct {
+	Host string `envconfig:"HOST" default:"localhost"`
+	Port string `envconfig:"PORT" default:"8080"`
+}
