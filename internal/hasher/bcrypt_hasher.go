743fd91be299ff32f721c64865e79168b5657ddb
DigiRon4ik <dr.digiron@gmail.com>
2025-02-15 00:04:32 +0300

    [feature-hasher] basic password hasher with bcrypt done

---
+++ internal/hasher/bcrypt_hasher.go
@@ -0,0 +1,32 @@
+package hasher
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+// BcryptHasher provides functionality for hashing and comparing passwords using bcrypt.
+type BcryptHasher struct{}
+
+// New creates a new instance of BcryptHasher.
+func New() *BcryptHasher {
+	return &BcryptHasher{}
+}
+
+// Hash generates a bcrypt hash of the provided password.
+func (h *BcryptHasher) Hash(passwd string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(passwd), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+
+// Compare checks if the provided password matches the hashed password.
+func (h *BcryptHasher) Compare(hashedPasswd, passwd string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPasswd), []byte(passwd))
+	if err != nil {
+		// If passwords do not match, return false.
+		return false
+	}
+	return true
+}
