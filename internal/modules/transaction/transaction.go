022e693f6fab4f4d6b90486b8480aff8e3b125ad
DigiRon4ik <dr.digiron@gmail.com>
2025-02-14 23:21:02 +0300

    [feature-transaction] basic transaction module is ready

--- internal/modules/transaction/transaction.go
+++ internal/modules/transaction/transaction.go
@@ -1,1 +1,43 @@
 package transaction
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+)
+
+// database interface defines methods for handling coin transactions and user data.
+type database interface {
+	GetIdByUsername(ctx context.Context, username string) (int, error)
+	GetCoinsByUserID(ctx context.Context, userID int) (int, error)
+	TransferCoins(ctx context.Context, fromUserID, toUserID, coins int) error
+}
+
+// TransactService provides functionality for handling coin transactions.
+type TransactService struct {
+	storage database
+}
+
+// New creates a new instance of TransactService with the given storage.
+func New(storage database) *TransactService {
+	return &TransactService{storage}
+}
+
+// GetIdRecipient retrieves the ID of a recipient by their username, handling database errors.
+func (s *TransactService) GetIdRecipient(ctx context.Context, username string) (int, error) {
+	id, err := s.storage.GetIdByUsername(ctx, username)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return id, err
+	}
+	return id, nil
+}
+
+// GetSenderCoins retrieves the number of coins a sender has by their ID.
+func (s *TransactService) GetSenderCoins(ctx context.Context, userID int) (int, error) {
+	return s.storage.GetCoinsByUserID(ctx, userID)
+}
+
+// SendCoinsToUser transfers coins from a sender to a recipient.
+func (s *TransactService) SendCoinsToUser(ctx context.Context, senderID, recipientID int, coins int) error {
+	return s.storage.TransferCoins(ctx, senderID, recipientID, coins)
+}
