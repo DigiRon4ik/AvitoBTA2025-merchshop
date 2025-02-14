d0787cb79d7ce80969b6539f96ceadc61d0740b3
DigiRon4ik <dr.digiron@gmail.com>
2025-02-14 20:00:15 +0300

    [feature-server] moved the dependencies for the handlers

--- internal/server/handlers/user.go
+++ internal/server/handlers/user.go
@@ -13,35 +13,6 @@ import (
 
 var ErrInDB = errors.New("something happened to the database")
 
-type authService interface {
-	GetOrRegUser(ctx context.Context, username string) (*models.User, bool, error)
-	ComparePassword(ctx context.Context, lPasswd, uPasswd string) bool
-}
-
-type tokenManager interface {
-	NewToken(userID, username string) (string, error)
-}
-
-type userInfoService interface {
-	GetCoins(ctx context.Context, userID int) (int, error)
-	GetInventory(ctx context.Context, userID int) (*[]models.Merch, error)
-	GetCoinHistory(ctx context.Context, userID int) (*models.CoinHistory, error)
-	// GetHistoryReceivingCoins(ctx context.Context, userID int) (*[]models.Receiving, error)
-	// GetHistorySendingCoins(ctx context.Context, userID int) (*[]models.Sending, error)
-}
-
-type transactionService interface {
-	GetIdRecipient(ctx context.Context, username string) (int, error)
-	GetSenderCoins(ctx context.Context, userID int) (int, error)
-	SendCoinsToUser(ctx context.Context, senderID, recipientID int, coins int) error
-}
-
-type buyItemService interface {
-	GetItem(ctx context.Context, slug string) (*models.Item, error)
-	GetBuyerCoins(ctx context.Context, userID int) (int, error)
-	BuyItem(ctx context.Context, userID int, item *models.Item) error
-}
-
 type UserHandlers struct {
 	ctx       context.Context
 	authSrv   authService
